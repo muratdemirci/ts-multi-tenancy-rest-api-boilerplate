ef2cbc2b5d20219f7f5b815225ddff199f1d5842
muratdemirci <6681137+muratdemirci@users.noreply.github.com>
2024-04-25 10:01:09 +0300

    Refactor zod-error-handler middleware to handle optional path property in error details

--- src/middlewares/zod-error-handler.middleware.ts
+++ src/middlewares/zod-error-handler.middleware.ts
@@ -11,19 +11,14 @@ interface IZodError extends Error {
   format?: () => IZodErrorDetail[];
 }
 
-export default (
-  err: IZodError,
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction,
-) => {
+export default (err: IZodError, req: express.Request, res: express.Response, next: express.NextFunction) => {
   if (err instanceof ZodError) {
     const error = {
       code: HttpStatus.BAD_REQUEST,
       message: HttpStatus.getStatusText(HttpStatus.BAD_REQUEST),
-      details: err.format().map(err => ({
+      details: err.format().map((err) => ({
         message: err.message,
-        param: err.path.join('.'),
+        param: err.path?.join('.') ?? '',
       })),
     };
     return res.status(HttpStatus.BAD_REQUEST).json(error);
