84d6e16831cf074b8d3344398e7b80bbda00645d
muratdemirci <6681137+muratdemirci@users.noreply.github.com>
2024-04-25 09:49:16 +0300

    Refactor user.grpc.ts to handle optional PROTO environment variable

--- src/grpc/user.grpc.ts
+++ src/grpc/user.grpc.ts
@@ -4,7 +4,8 @@ import userService from '../api/services/user/user.service';
 import { promiseCall } from '../utilities/grpc.utility';
 import path from 'path';
 
-const packageDefinition = loader.loadSync(path.join(process.env.PROTOS, 'user.proto'));
+const protosPath = process.env.PROTOS || ''; // Set a default value for process.env.PROTOS if it is undefined
+const packageDefinition = loader.loadSync(path.join(protosPath, 'user.proto'));
 const proto = grpc.loadPackageDefinition(packageDefinition) as any;
 
 export const init = async (server: any) => {
